546d503b161664ece376ed24ed14be29baf9b220
shahril2206 <naiseu0622@gmail.com>
2026-01-20 22:34:18 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -260,7 +260,7 @@ export default function Index() {
             </Link>
           </Button>
         </div>
-        <hr class="mx-auto w-4/5 border-gray-300" />
+        <hr class="mt-5 mx-auto w-9/10 border-gray-300" />
       </section>
 
       <Footer />
