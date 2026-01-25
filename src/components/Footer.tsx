5d8afc99e6809831df12fcdd2f6d93a061212fe8
shahril2206 <naiseu0622@gmail.com>
2026-01-25 16:54:32 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -119,7 +119,7 @@ export function Footer() {
 
         {/* Bottom */}
         <div className="pt-8 border-t border-background/10 flex flex-col md:flex-row justify-center md:justify-between items-center">
-          <p className="text-background/50 text-sm">
+          <p className="text-background/50 text-sm text-center">
             © {new Date().getFullYear()} Mosel. All rights reserved.
           </p>
         </div>
